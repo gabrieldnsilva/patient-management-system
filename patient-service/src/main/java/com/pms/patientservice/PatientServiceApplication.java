30999947ca9b5c0b85f9549f5065038433c4c43a
gabrieldnsilva <gabrieldnsilva@gmail.com>
2025-08-03 13:05:06 -0300

    feat: PatientController + api-requests (GET) from H2 database

--- patient-service/src/main/java/com/pms/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pms/patientservice/PatientServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientserviceApplication {
+public class PatientServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PatientserviceApplication.class, args);
+        SpringApplication.run(PatientServiceApplication.class, args);
     }
 
 }
